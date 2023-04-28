0e99e7ab61d69f7d82b0f10ea3cd2f21805ccd49
shawny17 <ases60517@gmail.com>
2023-04-28 15:03:19 +1200

    Add test.py and README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Files
+
+This is an example API tests using Python unittest test framework.
+
+## Prerequisites
+
+You'll need Python3 installed as well as module 'requests' installed.
+To install requests module run: `pip install requests`
+
+## How to Run
+
+Open terminal and run: `python test.py`
---
+++ test.py
@@ -0,0 +1,38 @@
+import requests
+import unittest
+
+"""
+API test class that utilise Python unittest framework
+"""
+class APITests(unittest.TestCase):
+
+    # Initial setup for the whole class
+    @classmethod
+    def setUpClass(cls):
+        # URL of the test
+        url = 'https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false'
+        # Get response body as JSON
+        cls.response = requests.get(url=url).json()
+
+    # TC-1: Verify that Name of response body is equal to 'Cardon credits'
+    def test_name_is_valid(self):
+        self.assertEqual(self.response['Name'], 'Carbon credits',
+                         "FAIL: Name element returns '{}'.".format(self.response['Name']))
+
+    # TC-2: Verify that CanRelist of response body is true
+    def test_can_relist_is_valid(self):
+        self.assertEqual(self.response['CanRelist'], True,
+                         "FAIL: CanRelist element returns '{}'.".format(self.response['CanRelist']))
+
+    # TC-3: Verify that Promotions element with Name = "Gallery" has a Description that 
+    # contains the text "Good position in category"
+    def test_promotion_gallery_contains_value(self):
+        for promotion in self.response['Promotions']:
+            if promotion['Name'] == 'Gallery':
+                self.assertEqual(promotion['Description'], 'Good position in category',
+                                 "FAIL: Description element returns '{}'.".format(promotion['Description']))
+
+# Main entry of test
+if __name__ == '__main__':
+    unittest.main()
+ 
